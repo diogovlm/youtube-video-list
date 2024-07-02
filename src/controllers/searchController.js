19c5182a5f4e399ae18c2c359132ec9e0a124f92
Diogo Vilela <diogovlm@gmail.com>
2024-07-02 01:15:58 -0300

    feat: add pagination to video search

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -2,8 +2,9 @@ const youtubeService = require('../services/youtubeService');
 
 exports.searchVideos = async (req, res) => {
     const query = req.query.query;
+    const pageToken = req.query.pageToken || '';
     try {
-        const data = await youtubeService.fetchYouTubeData(query);
+        const data = await youtubeService.fetchYouTubeData(query, pageToken);
         res.json(data);
     } catch (error) {
         res.status(500).send('Error fetching data from YouTube API');
