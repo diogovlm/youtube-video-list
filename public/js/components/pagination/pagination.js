5dd256362af807860a4f41762f75e284c565f3b6
Diogo Vilela <diogovlm@gmail.com>
2024-07-02 02:47:55 -0300

    fix: fix tab space

--- public/js/components/pagination/pagination.js
+++ public/js/components/pagination/pagination.js
@@ -1,25 +1,25 @@
 export function setupPagination(nextPageToken, prevPageToken, onPageChange) {
   fetch('/js/components/pagination/pagination.html')
-      .then(response => response.text())
-      .then(data => {
-          document.querySelector('.pagination-container').innerHTML = data;
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector('.pagination-container').innerHTML = data;
 
-          const nextButton = document.getElementById('nextPage');
-          const prevButton = document.getElementById('prevPage');
+      const nextButton = document.getElementById('nextPage');
+      const prevButton = document.getElementById('prevPage');
 
-          if (nextPageToken) {
-              nextButton.style.display = 'inline';
-              nextButton.onclick = () => onPageChange(nextPageToken);
-          } else {
-              nextButton.style.display = 'none';
-          }
+      if (nextPageToken) {
+        nextButton.style.display = 'inline';
+        nextButton.onclick = () => onPageChange(nextPageToken);
+      } else {
+        nextButton.style.display = 'none';
+      }
 
-          if (prevPageToken) {
-              prevButton.style.display = 'inline';
-              prevButton.onclick = () => onPageChange(prevPageToken);
-          } else {
-              prevButton.style.display = 'none';
-          }
-      })
-      .catch(error => console.error('Error loading pagination:', error));
+      if (prevPageToken) {
+        prevButton.style.display = 'inline';
+        prevButton.onclick = () => onPageChange(prevPageToken);
+      } else {
+        prevButton.style.display = 'none';
+      }
+    })
+    .catch(error => console.error('Error loading pagination:', error));
 }
--- src/app.js
+++ src/app.js
@@ -9,5 +9,5 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/', routes);
 
 app.listen(config.port, () => {
-    console.log(`Server is running on http://localhost:${config.port}`);
+  console.log(`Server is running on http://localhost:${config.port}`);
 });
