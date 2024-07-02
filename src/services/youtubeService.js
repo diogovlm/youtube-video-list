6076e67278c28cc00d949f1a29a61759868ce738
Diogo Vilela <diogovlm@gmail.com>
2024-07-02 01:15:58 -0300

    feat: add pagination to video search

--- src/services/youtubeService.js
+++ src/services/youtubeService.js
@@ -1,8 +1,17 @@
 const axios = require('axios');
 const config = require('../config/config');
 
-exports.fetchYouTubeData = async (query) => {
-    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${query}&key=${config.youtubeApiKey}`;
-    const response = await axios.get(apiUrl);
+exports.fetchYouTubeData = async (query, pageToken = '') => {
+    const apiUrl = `https://www.googleapis.com/youtube/v3/search`;
+    const response = await axios.get(apiUrl, {
+      params: {
+        part: 'snippet',
+        maxResults: 16,
+        q: query,
+        type: 'video',
+        key: config.youtubeApiKey,
+        pageToken: pageToken
+      }
+    });
     return response.data;
 };
