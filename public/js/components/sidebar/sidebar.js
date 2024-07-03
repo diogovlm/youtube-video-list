6b97484575b06d8a2011242a45e07b22242641d1
Diogo Vilela <diogovlm@gmail.com>
2024-07-03 03:48:15 -0300

    feat: add video to mongodb collection when clicked

--- public/js/components/sidebar/sidebar.js
+++ public/js/components/sidebar/sidebar.js
@@ -1,25 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA5se2cKozb_3mtmOwx8EPoj5_oq6Hd0RA",
-  authDomain: "video-lists-cdea6.firebaseapp.com",
-  projectId: "video-lists-cdea6",
-  storageBucket: "video-lists-cdea6.appspot.com",
-  messagingSenderId: "845100579215",
-  appId: "1:845100579215:web:ca5fa8b57cc7f1cca973fa",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
+import { auth } from '../../firebase.js'
 
 export function setupSidebar() {
   fetch('/js/components/sidebar/sidebar.html')
--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
 const config = require('./config/config');
-const routes = require('./routes/index');
+const apiRoutes = require('./routes/apiRoutes');
+const pageRoutes = require('./routes/pageRoutes');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/', routes);
+
+app.use('/api', apiRoutes);
+app.use('/', pageRoutes);
 
 app.listen(config.port, () => {
   console.log(`Server is running on http://localhost:${config.port}`);
